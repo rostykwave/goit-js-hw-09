57a5d177b7497d03a1e218aeadcf2d4c6c6df6bc
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-24 16:43:57 +0300

    clear look

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,17 +6,6 @@ const refs = {
 
 
 refs.form.addEventListener('submit', onFormSubmit);
-////delay
-////step - delay+=step on every iteration
-/////amount - (of iterations)
-////position - number of iertation (from 1)
-
-///1-2-3
-//get input from formData
-//iteration amount times
-//make promise
-//increase delay on+step
-//notiflix
 
   ////Functions
 
@@ -24,9 +13,7 @@ function onFormSubmit(event) {
     event.preventDefault();
 
     const formData = {};
-
     const formDataRaw = new FormData(event.currentTarget);
-
     ///перебір масиву формдати і додавання елементів в об'єкт
     formDataRaw.forEach((value, name) => {
         formData[name] = value;
@@ -39,31 +26,21 @@ function onFormSubmit(event) {
     const amount = Number(formData.amount);
     const step = Number(formData.step);
 
+
     ////Цикл
-    for (let i = 0; i < amount; i += 1) {
-        ///встановлення позиції по-порядку від одиниці, а не від 0
-        const position = i+1;
-
-
-            ////виклик функції з промісами
-    createPromise(position, delay).then(({ position, delay }) => {
-    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-        
-        
-        ///збільшення затримки на крок степ після виконання функції
+    for (let i = 1; i <= amount; i += 1) {
+    ////виклик функції з промісами
+        createPromise(i, delay).then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+        ///збільшення затримки на крок степ після кожної ітерації і виконання функції
            delay += step;
-        
     }
  
 }
-
-
 
 
 function createPromise(position, delay) {
